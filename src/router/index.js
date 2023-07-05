bd2ae7c69a2b3b3c76a98138374a9d469e5c5ca4
邵励治 <shaolizhi1234@126.com>
2023-07-05 12:28:37 +0800

    增加 Google Analytics

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import TaskView from "../views/TaskView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView
-    },
-    {
-      path: "/task",
       name: "task",
-      component: () => import("../views/TaskView.vue")
+      component: TaskView
     }
+    // {
+    //   path: "/task",
+    //   name: "task",
+    //   component: () => import("../views/TaskView.vue")
+    // }
   ]
 });
 
