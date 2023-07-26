08473d33cef9000bb561f14fb0d70746d1d0598a
邵励治 <shaolizhi1234@126.com>
2023-07-27 01:03:44 +0800

    增加了分割章节的函数 splitChapters

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -55,6 +55,43 @@ import { computed, ref, onMounted, watchEffect } from "vue";
 import Papa from "papaparse";
 import Api from "@/model/api.js";
 import { ElMessage } from "element-plus";
+
+let book = `第一部分 智人征服世界
+人类与其他动物有何不同？
+人类如何征服世界？
+智人究竟是比较高级的生命形式，还是欺凌其他物种的地痞
+流氓？
+第1章 人类的新议题
+第三个千年开始之际，人类醒来，伸展手脚，揉了揉眼睛，
+脑子里依然萦绕着某些可怕的噩梦。“好像有什么铁丝网、巨大
+的蘑菇云之类的。但管它的呢，只是个噩梦吧。”人类走进浴
+室，洗洗脸，看看镜子里脸上的皱纹，然后冲了一杯咖啡，打开
+了记事本。“来瞧瞧今天有什么重要的事吧。”
+几千年来，这个问题的答案并没有什么改变。不管是20世纪
+的中国人、中世纪的印度人，还是古代的埃及人，都面临着同样
+的三大问题：饥荒、瘟疫和战争，它们永远都是人类的心头大
+患。一代又一代，人类向所有神明、天使和圣人祈祷膜拜，也发
+明了无数的工具、制度和社会系统，但仍然有数百万人死于饥
+饿、流行病和暴力。许多思想家和先知于是认为，饥荒、瘟疫和
+战争一定是上帝整个宇宙计划的一部分，抑或是由于人类天生的
+不完美，除非走到时间尽头，否则永远不可能摆脱。`;
+
+function splitChapters(book) {
+  let chapters = book.split(
+    /(第\s*[\d|\p{Script=Han}一二三四五六七八九十百千万亿兆]+\s*章)/g
+  );
+  // 通过正则表达式我们已经可以正确地分割章节，但是我们还需要重新组合章节名和章节内容
+  let formattedChapters = [];
+  formattedChapters.push(chapters[0]);
+  for (let i = 1; i < chapters.length; i += 2) {
+    // 第一次循环时，我们跳过开头可能存在的空字符串
+    // 将章节名和章节内容重新组合，注意这里我们需要跳过空的章节
+    formattedChapters.push(chapters[i] + chapters[i + 1]);
+  }
+  return formattedChapters;
+}
+
+console.log(splitChapters(book));
 // Input 元素
 const inputCsv = ref(null);
 
